e4b60d9cc90ea1436aa2aa197d2e9b8fd0db6a40
MannJefferson <mannjefferson84@gmail.com>
2023-06-03 21:33:33 +0200

    JS app and Products created

---
+++ shopping cart/products.js
@@ -0,0 +1,133 @@
+const products = [
+    {
+      id: 0,
+      name: "P-station 1",
+      price: 65000,
+      instock: 20,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port1.jpg",
+    },
+    {
+      id: 1,
+      name: "P-station 2",
+      price: 8000,
+      instock: 50,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port2.jpg",
+    },
+    {
+      id: 2,
+      name: "P-station 3",
+      price: 12000,
+      instock: 15,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port3.jpg",
+    },
+    {
+      id: 3,
+      name: "P-station 4",
+      price: 15990,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port4.png",
+    },
+    {
+      id: 4,
+      name: "P-station 5",
+      price: 4500,
+      instock: 15,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port5.jpg",
+    },
+    {
+      id: 5,
+      name: "P-station 6",
+      price: 6000,
+      instock: 30,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/port6.jpg",
+    },
+  
+    {
+      id: 6,
+      name: "generator 1",
+      price: 12500,
+      instock: 20,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/gen1.jpeg",
+    },
+    {
+      id: 7,
+      name: "generator 2",
+      price: 12500,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/gen2.jpeg",
+    },
+    {
+      id: 8,
+      name: "generator 3",
+      price: 14500,
+      instock: 4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/gen3.jpeg",
+    },
+    {
+      id: 9,
+      name: "generator 14",
+      price: 35000,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/gen4.jpg",
+    },
+  
+    
+    {
+      id: 10,
+      name: "lights Rechargable",
+      price: 19.99,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/accessory1.jpg",
+    },
+    {
+      id: 11,
+      name: "Lights",
+      price: 100,
+      instock: 75,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/accessory2.jpg",
+    },
+    {
+      id: 12,
+      name: "Gas Heater",
+      price: 2150,
+      instock: 14,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/accessory3.jpg",
+    },
+    {
+      id: 13,
+      name: "Gas Stove",
+      price: 4500,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "./img/accessory4.png",
+    },
+  
+  ];
+  
